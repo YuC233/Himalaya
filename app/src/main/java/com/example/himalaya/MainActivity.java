5ea99d9d6f04162e2e2c454e2859e7ef8ee3fd45
t_benjaminhe <t_benjaminhe@auto-pai.com>
2020-07-08 18:26:11 +0800

    配置SDK,网络请求失败

--- app/src/main/java/com/example/himalaya/MainActivity.java
+++ app/src/main/java/com/example/himalaya/MainActivity.java
@@ -3,14 +3,43 @@ package com.example.himalaya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-@Override
+        private static final String TAG = "hyc";
+
+        @Override
 protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        Map<String, String> map = new HashMap<String, String>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+                @Override
+                public void onSuccess(CategoryList categoryList) {
+                        List<Category> categories = categoryList.getCategories();
+                        if (categories != null) {
+                                int size = categories.size();
+                                Log.e(TAG, "categories size --->" + size);
+                                for (Category category : categories) {
+                                        Log.e(TAG,"category --->" + category.getCategoryName());
+                                }
+                        }
+                }
+                @Override
+                public void onError(int i, String s) {
+                        Log.e(TAG,"error code --->" + i + "error msg --->" + s);
         }
+});
+        }
+
 }
